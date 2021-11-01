0f9e2c1cea6bba841b4a81d99928b5fb25872a90
miatang13 <miatang13@gmail.com>
2021-10-31 22:04:31 -0400

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ export default function NavigationBar() {
       <div id="navigation__container">
         <div id="nav__content">
           <div class="column narrow__left" id="nav__content__left">
-            <NavLink className="nav__route" to="/">
+            <NavLink className="nav__route" to="/CDT-project-01">
               <span id="logo__text">Nova</span>
             </NavLink>
           </div>
